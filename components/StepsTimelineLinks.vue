74709f22375b7cf2066df2d5030bef6cf242697e
Jesús García <ctw@ctwhome.com>
2021-06-23 15:35:16 +0200

    feat: add timeline buttons for pages

--- components/StepsTimelineLinks.vue
+++ components/StepsTimelineLinks.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="container">
-    <div v-if="items" class="container">
-      <explore-stories-button />
-
-      <div v-for="(story,i) in items" :key="i" class="flex flex-col md:grid mx-auto text-blue-50">
+    <div v-if="pages" class="container">
+      <div v-for="(page,i) in pages" :key="i" class="flex flex-col md:grid mx-auto text-blue-50">
         <div class="mr-10 md:mx-auto relative h-20">
           <div class="h-full w-6 flex items-center justify-center">
             <div class="h-full w-1 bg-gray-800" />
@@ -11,16 +9,17 @@
           <div class="w-6 h-6 absolute top-1/4 -mt-3 flex justify-center">
             <div class="relative py-3 sm:max-w-xl sm:mx-auto">
               <div class="group cursor-pointer relative inline-block text-center">
-                <nuxt-link :to="story.route" class="btn btn-circle text-white">
-                  {{ story.title }}
+                <nuxt-link tag="div" :to="'/stories' + page.path" class="btn btn-circle text-white">
+                  {{ i + 1 }}
                 </nuxt-link>
                 <div
                   class=" w-[170px] h-14 flex items-center justify-center bg-gray-800 text-white text-center rounded-lg py-2
-                              absolute opacity-0 transition-all
-                              z-10 group-hover:opacity-100 bottom-full mr-0 px-3 pointer-events-none
-                              transform translate-x-[-200px] translate-y-[55px]"
+                          absolute opacity-0 transition-all
+                          z-10 group-hover:opacity-100 bottom-full mr-0 px-3 pointer-events-none
+                          transform translate-x-[-200px] translate-y-[55px]
+                "
                 >
-                  {{ story.tooltip }}
+                  {{ page.title }}
                 </div>
               </div>
             </div>
@@ -34,8 +33,7 @@
 <script>
 
 export default {
-  props: ['items']
-
+  props: ['pages']
 }
 
 </script>
