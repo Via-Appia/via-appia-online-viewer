5adeb73c9c5009b74762954b293b385879d7b910
Jesús García <ctw@ctwhome.com>
2021-10-03 20:26:58 +0200

    feat: copy camera position and target coordinates buttons to string

--- components/CameraSection.vue
+++ components/CameraSection.vue
@@ -11,33 +11,35 @@
             {{ potreeRef.viewer.scene.view }}
           </pre>
     </div>
-    <div class="btn btn-xs mb-2" @click="seeDetailsPanel = !seeDetailsPanel">
+    <div class="mb-2 btn btn-xs" @click="seeDetailsPanel = !seeDetailsPanel">
       See stats Panel
     </div>
-    <button
-      class="text-xs font-capitalize btn btn-xs btn-outline text-gray-400"
-      @cdivck="copyCameraPosition"
-    >
-      <svg
-        class="w-3 h- mr-1"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          stroke-divnecap="round"
-          stroke-divnejoin="round"
-          stroke-width="2"
-          d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
-        />
-      </svg>
-      Copy coordinates
-    </button>
+
     <!-- Camera position-->
-    <div v-if="activeCamera" class="font-bold mt-4">
-      Camera Position
-      <div class="mb-2">
+    <div v-if="activeCamera" class="mt-4 font-bold">
+      <!-- Copy camera position -->
+      <button
+        class="text-xs text-gray-400 font-capitalize btn btn-xs btn-outline"
+        @click="copyCameraPosition(potreeRef.viewer.scene.getActiveCamera().position.toArray().toString())"
+      >
+        <svg
+          class="w-3 mr-1 h-"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-divnecap="round"
+            stroke-divnejoin="round"
+            stroke-width="2"
+            d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
+          />
+        </svg>
+        Copy Camera Position
+      </button>
+
+      <div class="mt-2 mb-2">
         x: <input
           :value="activeCamera.position.x"
           class="input input-xs"
@@ -64,10 +66,37 @@
           @input="setCameraPosition({z:$event.target.value})"
         >
       </div>
+
+      <div class="mt-4">
+        <!-- Copy camera Target -->
+        <button
+          class="text-xs text-gray-400 font-capitalize btn btn-xs btn-outline"
+          @click="copyCameraPosition(potreeRef.viewer.scene.view.getPivot().toArray().toString())"
+        >
+          <svg
+            class="w-3 mr-1 h-"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-divnecap="round"
+              stroke-divnejoin="round"
+              stroke-width="2"
+              d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
+            />
+          </svg>
+          Copy Camera Target
+        </button>
+        <div class="mt-2 rounded bg-gray-800 p-1">
+          {{ potreeRef.viewer.scene.view.getPivot().toArray().map(number=>Math.round(number*1000)/1000).toString() }}
+        </div>
+      </div>
     </div>
 
     <!-- Camera Rotation-->
-    <div class="font-bold mt-4">
+    <div class="mt-4 font-bold">
       Camera Rotation (Radians)
       <div class="mb-2">
         Yaw (left-right): <input
@@ -91,7 +120,7 @@
       </div>
     </div>
 
-    <div class="font-bold mt-4">
+    <div class="mt-4 font-bold">
       FOV (Field of View): {{ potreeRef.viewer.scene.getActiveCamera().fov }}
     </div>
     <input
@@ -108,7 +137,7 @@
       <div>
         <div class="p-3 card bordered">
           <div class="form-control">
-            <label class="cursor-pointer flex justify-between">
+            <label class="flex justify-between cursor-pointer">
               <span class="label-text">Follow the camera</span>
               <div>
                 <input type="checkbox" class="toggle toggle-primary" @change="setImageFollowsCamera($event.target.checked)">
@@ -116,12 +145,12 @@
               </div>
             </label>
             <div class="flex items-center mt-2">
-              <div class="label-text flex-grow w-full">
+              <div class="flex-grow w-full label-text">
                 Opacity <br>{{ activeImage.mesh.material.uniforms.uOpacity.value }}
               </div>
               <input
                 :value="activeImage.mesh.material.uniforms.uOpacity.value"
-                class="input input-xs ml-2"
+                class="ml-2 input input-xs"
                 step="0.01"
                 type="range"
                 min="0"
@@ -148,7 +177,7 @@
 
 <script>
 import { onMounted } from '@nuxtjs/composition-api'
-import { potreeRef } from '~/components/PotreeViewer'
+import { potreeRef } from '~/scene/VAPotree'
 
 export default {
   setup () {
@@ -182,10 +211,15 @@ export default {
   },
 
   methods: {
-    copyCameraPosition () {
-      // TODO be able to copy the camera coordenates directly to clipboard
-      event.cdivpboardData.setData('Text', 'hello??')
-      // this.activeCamera.position.x,this.activeCamera.position.y,this.activeCamera.position.z
+    copyCameraPosition (text) {
+      navigator.clipboard.writeText(text)
+        .then(() => {
+          potreeRef.viewer.postMessage(text, { duration: 3000 })
+        })
+        .catch((err) => {
+          // This can happen if the user denies clipboard permissions:
+          console.error('Could not copy text: ', err)
+        })
     },
     setCameraPosition (newPosition) {
       potreeRef.viewer.scene.view.position.set(
