5ca32d86d6b1eacb969660cf579ec93f8b331445
Jorne Envy <jornejongsma@gmail.com>
2022-01-25 23:46:48 +0100

    test

--- api/websocket.js
+++ api/websocket.js
@@ -4,3 +4,42 @@ export const socket = new WebSocket('ws://localhost:8080')
 socket.onmessage = (data) => {
   console.log('🔥 messge at the server', data)
 }
+
+/* 
+const slug = window.location.href.split('#')[1]
+
+class Socket extends WebSocket {
+  constructor (id) {
+    const host = require('os')
+      .hostname()
+      .toLowerCase()
+    const port = 8001
+    const url = `ws://${host}:${port}`
+    super(url)
+    console.log('ID: ', id)
+    this.id = id
+  }
+
+  // send (obj) {
+  //   super.send(JSON.stringify(obj))
+  // }
+
+  connect () {
+    this.send(JSON.stringify({ type: 'CONNECT', payload: { id: this.id } }))
+  }
+
+  // startView(view) {
+  //   this.send({ type: "START-VIEW", payload: view });
+  // }
+
+  // endView(view) {}
+}
+
+export const socket = new Socket(slug)
+
+socket.onopen = (event) => {
+  socket.connect()
+}
+ */
+
+// export default Socket;
