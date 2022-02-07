d325817aa2941d25bc7bacabf110c4586b3af286
Jesús García <ctw@ctwhome.com>
2022-02-07 20:34:12 +0100

    feat: only show relevant locales

--- components/LocaleSwitch.vue
+++ components/LocaleSwitch.vue
@@ -1,33 +1,44 @@
 <template>
   <div class="select-none">
     <span
+      v-if="langs.nl"
       class="cursor-pointer hover:underline"
       :class="{'primary-text':potreeRef.lang==='nl'}"
       @click="changeLocale('nl')"
     >
       nl
     </span>
-    |
+
     <span
+      v-if="langs.en"
       class="cursor-pointer hover:underline"
       :class="{'primary-text':potreeRef.lang==='en'}"
       @click="changeLocale('en')"
     >
-      en
+      | en
     </span>
-    |
+
     <span
+      v-if="langs.de"
       class="cursor-pointer hover:underline"
       :class="{'primary-text':potreeRef.lang==='de'}"
       @click="changeLocale('de')"
     >
-      de
+      | de
     </span>
   </div>
 </template>
 
 <script setup>
+import { defineProps } from '@nuxtjs/composition-api'
 import { potreeRef } from '~/api/VAPotree'
+
+defineProps({
+  langs: {
+    type: Object,
+    default: () => ({ nl: true, en: true, de: true })
+  }
+})
 function changeLocale (locale) {
   potreeRef.lang = locale
 }
